90a373a42b1007f1d916c061163db7f90e52d19b
juniorctutoriales <junior_arcanjel@hotmail.com>
2015-07-31 20:37:01 -0500

    Update JuniorcLauncher.user.js

--- JuniorcLauncher.user.js
+++ JuniorcLauncher.user.js
@@ -1294,7 +1294,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["Prueba"],
+                names = ["JuniorcBot"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
